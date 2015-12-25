cc4324026ea37532941f42d1476781c6fc5d5b10
David Cormier <david.cormier@erudit.org>
2015-12-25 11:18:11 -0500

    remove legacy code
    Remove code that was used by django-plupload but not
    by django-resumable-files

--- plupload/urls.py
+++ plupload/urls.py
@@ -13,17 +13,4 @@ urlpatterns = patterns(
     # the url where the upload petition is processed
     url(r'^plupload/', 'plupload.views.upload_file',
         name='upload_file'),
-
-    # The folowing are the urls for a custom upload queue
-    url(r'^custom_queue$', 'plupload.views.upload_custom',
-        name='upload_custom'),
-
-    url(r'^del_file/(\d{4})/(\d{2})/(\d+)/$',
-        'plupload.views.del_file',
-        name='upload_del'),
-
-    # retrieves a list of all files in a dir, change as convenient
-    url(r'^get_all_files/(\d{4})/(\d{2})/(\d+)/$',
-        'plupload.views.get_all_files',
-        name="get_all_files"),
 )
