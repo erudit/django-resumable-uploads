349acfe8fca8716ec7129a2951f62d593dea006b
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-08-22 12:51:21 -0400

    Remove Django 1.10 warnings

--- plupload/urls.py
+++ plupload/urls.py
@@ -1,20 +1,16 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import views
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
+    # the url where the upload petition is processed
+    url(r'^plupload/resumable-file/(?P<pk>[\d-]+)/delete/$',
+        views.ResumableFileDeleteView.as_view(), name='resumable_file_delete'),
 
     # the url where the upload petition is processed
-    url(r'^plupload/resumable-file/(?P<pk>[\d-]+)/delete/$', views.ResumableFileDeleteView.as_view(),
-        name='resumable_file_delete'),
+    url(r'^plupload/set_file_info', views.set_file_info, name='set_file_info'),
 
     # the url where the upload petition is processed
-    url(r'^plupload/set_file_info', 'plupload.views.set_file_info',
-        name='set_file_info'),
-
-    # the url where the upload petition is processed
-    url(r'^plupload/', 'plupload.views.upload_file',
-        name='upload_file'),
-)
+    url(r'^plupload/', views.upload_file, name='upload_file'),
+]
