e5cb2ca1fe2a3e030bbbaca442dd9e0aad12c2ad
David Cormier <david.cormier@erudit.org>
2017-01-24 11:19:47 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-plupload",
-    version="0.5",
+    version="0.6.0",
     description="""
 django-plupload is a barebones multi file upload app for django.
 Uses plupload""",
