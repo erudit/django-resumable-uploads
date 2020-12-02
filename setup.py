148fd932e7ea4b8eec32b94293f28687a26213aa
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-12-02 08:41:21 -0500

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-resumable-uploads",
-    version="1.0.10",
+    version="1.0.11",
     description="""
 django-resumable-uploads is a multi file upload app for django.
 Uses plupload""",
